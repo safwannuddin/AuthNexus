454d9189c3f1b3d253886e2fb0302d2ebc754d29
Mohd Safwan Uddin <160422733177@mjcollege.ac.in>
2025-04-21 21:14:16 +0530

    removed all the files

--- backend/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .auth import router as auth_router
-from .documents import router as documents_router
-from .dashboard import router as dashboard_router
-
-__all__ = ["auth_router", "documents_router", "dashboard_router"]
--- backend/services/automation/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Automation services package for AuthNexus.
-Contains the automation agent and integration with n8n workflows.
-"""
-from .n8n_integration import automation_service
-from .agent import automation_agent
-
-__all__ = ["automation_service", "automation_agent"]
